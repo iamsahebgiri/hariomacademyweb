e364e78956af67f2213d9cb9f8b9966e26195a19
Saheb Giri <iamsahebgiri>
2021-07-20 20:53:39 +0530

    ‼️ BREAKING: Removed everything

--- theme/components/heading.js
+++
@@ -1,9 +0,0 @@
-import { mode } from '@chakra-ui/theme-tools';
-
-const Heading = {
-  baseStyle: (props) => ({
-    color: mode('gray.800', 'gray.100')(props),
-  }),
-};
-
-export default Heading;
